5359fc65064eb60d35b5cd6830f9a184cba90707
Russkazkin <ruslan@skazkin.su>
2020-08-30 09:17:42 +0300

    Generate phpdocs for models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Post
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Post newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Post newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Post query()
+ * @mixin \Eloquent
+ */
 class Post extends Model
 {
     //

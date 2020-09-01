d7eb85357160ee4b3306edd036e9d5d2dbdb1246
Russkazkin <ruslan@skazkin.su>
2020-09-01 09:44:40 +0300

    Add RetrievePostTest

---
+++ tests/Feature/RetrievePostsTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RetrievePostsTest extends TestCase
+{
+    /** @test */
+    public function a_user_can_retrieve_posts()
+    {
+        $this->actingAs($user = factory(User::class)->create());
+        $posts = factory(Post::class, 2)->create();
+
+        $response = $this->get('/api/posts');
+
+        $response->assertStatus(200)->assertJson([
+            'data' => [
+                [
+                    'data' => [
+                        'type' => 'posts',
+                        'post_id' => $posts->first()->id,
+                        'attributes' => [
+                            'body' => $posts->first()->body,
+                        ]
+                    ],
+                ],
+                [
+                    'data' => [
+                        'type' => 'posts',
+                        'post_id' => $posts->first()->id,
+                        'attributes' => [
+                            'body' => $posts->first()->body,
+                        ]
+                    ],
+                ],
+            ],
+            'links' => [
+                'self' => url('/posts'),
+            ]
+        ]);
+    }
+}

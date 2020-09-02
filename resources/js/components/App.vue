ddff15eeda67b3f17763f99dc6dd245e123df86a
Russkazkin <ruslan@skazkin.su>
2020-09-02 12:58:48 +0300

    Add sidebar markup

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="flex flex-col flex-1 h-screen overflow-y-hidden">
         <Nav/>
-        <router-view/>
+        <div class="flex overflow-y-hidden flex-1">
+            <div class="w-1/3">Sidebar</div>
+            <div class="w-2/3 overflow-x-hidden">
+                <router-view/>
+                <p v-for="i in 100">Morbi ac felis. Nam eget dui. Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Donec sodales sagittis magna. Curabitur nisi.</p>
+            </div>
+        </div>
     </div>
 </template>
 

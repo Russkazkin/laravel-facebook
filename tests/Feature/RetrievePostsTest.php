03c44ada1570c718f5d0f4b9e61907fde04db702
Russkazkin <ruslan@skazkin.su>
2020-09-03 09:20:57 +0300

    Add posted_at to post resource

--- tests/Feature/RetrievePostsTest.php
+++ tests/Feature/RetrievePostsTest.php
@@ -28,6 +28,7 @@ class RetrievePostsTest extends TestCase
                         'post_id' => $posts->last()->id,
                         'attributes' => [
                             'body' => $posts->last()->body,
+                            'posted_at' => $posts->last()->created_at->diffForHumans(),
                         ]
                     ],
                 ],
@@ -37,6 +38,7 @@ class RetrievePostsTest extends TestCase
                         'post_id' => $posts->first()->id,
                         'attributes' => [
                             'body' => $posts->first()->body,
+                            'posted_at' => $posts->first()->created_at->diffForHumans(),
                         ]
                     ],
                 ],

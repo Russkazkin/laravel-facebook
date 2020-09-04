9697ca7c75612309bed902382067eddbab3d3367
Russkazkin <ruslan@skazkin.su>
2020-09-04 13:45:42 +0300

    Add my user to the seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,6 +12,18 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $user = User::where('email', 'ruslan@skazkin.su')->first();
+
+        if(!$user) {
+            User::create([
+                'name' => 'admin',
+                'email' => 'ruslan@skazkin.su',
+                'password' => '$2y$10$Hv7RJYtTrhrSTcgFVKcz0.aUfVWkQIBB13BpTbHTrknSkwgpQzvim',
+                'email_verified_at' => now(),
+                'remember_token' => Str::random(10),
+            ]);
+        }
+
         factory(User::class, 5)->create();
     }
 }

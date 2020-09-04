d05225e6234a8280121c55925487bd194ca7bc51
Russkazkin <ruslan@skazkin.su>
2020-09-04 13:50:54 +0300

    Add loading state for async request for the posts

--- resources/js/views/NewsFeed.vue
+++ resources/js/views/NewsFeed.vue
@@ -1,7 +1,8 @@
 <template>
 <div class="flex flex-col items-center py-4">
     <NewPost/>
-    <Post v-for="post in posts" :key="post.data.post_id" :post="post"/>
+    <p v-if="loading">Loading posts...</p>
+    <Post v-for="post in posts" :key="post.data.post_id" :post="post" v-else />
 </div>
 </template>
 
@@ -18,6 +19,7 @@ export default {
     data() {
         return {
             posts: null,
+            loading: true,
         }
     },
     async mounted() {
@@ -26,7 +28,7 @@ export default {
         } catch (error) {
             console.log('Unable to fetch posts, ' + error.status)
         } finally {
-
+            this.loading = false;
         }
     },
 }

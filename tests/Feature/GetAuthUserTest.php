970051daef4f8d5f482ce3298cce8dc05dbe60e8
Russkazkin <ruslan@skazkin.su>
2020-09-08 12:50:35 +0300

    Add GetAuthUserTest

---
+++ tests/Feature/GetAuthUserTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class GetAuthUserTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test */
+    public function authenticated_user_can_be_fetched ()
+    {
+        $this->actingAs($user = factory(User::class)->create());
+
+        $response = $this->get('/api/auth-user');
+
+        $response->assertStatus(200)->assertJson([
+            'data' => [
+                'attributes' => [
+                    'name' => $user->name,
+                    'user_id' => $user->id,
+                ],
+            ],
+            'links' => [
+                'self' => url('/user/' . $user->id),
+            ],
+        ]);
+    }
+}

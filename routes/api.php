96b0bf70d3e17517550eea85dc8ac9e819736307
Russkazkin <ruslan@skazkin.su>
2020-09-15 11:12:03 +0300

    Add a_user_can_send_a_friend_request test and a route

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->group(function (){
 
     Route::get('auth-user', 'AuthUserController@show');
+    Route::post('friend-request/{user}', 'FriendRequestController');
 
     Route::resources([
         'posts' => 'PostController',
---
+++ tests/Feature/FriendsTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class FriendsTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test  */
+    public function a_user_can_send_a_friend_request()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->actingAs($user = factory(User::class)->create(), 'api');
+        $anotherUser = factory(User::class)->create();
+
+        $responce = $this->post('/api/friend-request', [
+            'friend_id' => $anotherUser->id,
+        ]);
+
+        $responce->assertStatus(200);
+    }
+}

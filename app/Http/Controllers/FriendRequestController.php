a295957a7e6be12d63fc3e0bb1d5df08a1fd52e6
Russkazkin <ruslan@skazkin.su>
2020-09-15 11:31:37 +0300

    Complete store method for FriendRequestController

--- app/Http/Controllers/FriendRequestController.php
+++ app/Http/Controllers/FriendRequestController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Friend;
 use Illuminate\Http\Request;
 
 class FriendRequestController extends Controller
 {
    public function store()
    {
+        $data = request()->validate([
+            'friend_id' => '',
+        ]);
 
+        Friend::create($data);
    }
 }

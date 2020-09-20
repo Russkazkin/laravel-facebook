74cfef7098c93d80060793de2f7a94bf13383d38
Russkazkin <ruslan@skazkin.su>
2020-09-20 17:20:25 +0300

    Add only_the_recipient_can_accept_a_friend_request test

--- tests/Feature/FriendsTest.php
+++ tests/Feature/FriendsTest.php
@@ -127,4 +127,35 @@ class FriendsTest extends TestCase
             ],
         ]);
     }
+
+    /** @test  */
+    public function only_the_recipient_can_accept_a_friend_request()
+    {
+        $this->actingAs($user = factory(User::class)->create(), 'api');
+        $anotherUser = factory(User::class)->create();
+
+        $this->post('/api/friend-request', [
+            'friend_id' => $anotherUser->id,
+        ])->assertStatus(200);;
+
+        $response = $this->actingAs(factory(User::class)->create(), 'api')
+            ->post('/api/friend-request-response', [
+                'user_id' => $user->id,
+                'status' => 1,
+            ]);
+
+        $response->assertNotFound();
+
+        $friendRequest = Friend::first();
+        $this->assertNull($friendRequest->confirmed_at);
+        $this->assertNull($friendRequest->status);
+
+        $response->assertJson([
+            'errors' => [
+                'code' => 404,
+                'title' => 'Friend Request Not Found',
+                'detail' => 'Unable to locate the friend request with the given information.',
+            ],
+        ]);
+    }
 }

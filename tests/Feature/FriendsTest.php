93b804c437e698c948ec014fdd08cb189b299ade
Russkazkin <ruslan@skazkin.su>
2020-10-05 11:41:13 +0300

    Add unique constraint into friends table migration

--- tests/Feature/FriendsTest.php
+++ tests/Feature/FriendsTest.php
@@ -48,6 +48,8 @@ class FriendsTest extends TestCase
     public function a_user_can_send_a_friend_request_only_once()
     {
 
+        $this->withoutExceptionHandling();
+
         $this->actingAs($user = factory(User::class)->create(), 'api');
         $anotherUser = factory(User::class)->create();
 

243c44be62febf3dee4149702a007b78ca3a096b
Russkazkin <russkazkin@gmail.com>
2020-10-07 16:54:05 +0300

    Add a_user_can_like_a_post test

---
+++ tests/Feature/LikesTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Post;
+use App\Models\User;
+
+test('a_user_can_like_a_post', function () {
+    /* @var \Tests\TestCase $this */
+
+    $this->withoutExceptionHandling();
+    $this->actingAs($user = factory(User::class)->create());
+    $anotherUser = factory(User::class)->create();
+    $post = factory(Post::class)->create(['id' => 123, 'user_id' => $anotherUser->id]);
+    $response = $this->post('/api/post/' . $post->id . '/like/');
+    $response->assertOk();
+    $this->assertCount(1, $user->likedPosts);
+    $response->assertJson([
+        'data' => [
+            [
+                'data' => [
+                    'type' => 'likes',
+                    'like_id' => 1,
+                    'attributes' => [],
+                ],
+                'links' => [
+                    'self' => url('/posts'),
+                ]
+            ]
+        ],
+        'links' => [
+            'self' => url('/posts'),
+        ]
+    ]);
+});

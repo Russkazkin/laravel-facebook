cb4c726f20442ac849af9989056f59a1c8aa1485
Russkazkin <russkazkin@gmail.com>
2020-10-09 09:11:45 +0300

    Add comment model, migration and relation

--- app/Http/Controllers/PostCommentController.php
+++ app/Http/Controllers/PostCommentController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostCommentController extends Controller
 {
-    public function store()
+    public function store(Post $post)
     {
+        $data = request()->validate([
+            'body' => '',
+        ]);
 
+        $post->comments()->create($data);
     }
 }

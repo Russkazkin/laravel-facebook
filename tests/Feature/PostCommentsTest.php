edc88488d164183ffab7f3a5bae4a271f9fac609
Russkazkin <russkazkin@gmail.com>
2020-10-09 08:54:33 +0300

    Add a_user_can_comment_on_a_post test

---
+++ tests/Feature/PostCommentsTest.php
@@ -0,0 +1,47 @@
+<?php
+
+test('a_user_can_comment_on_a_post', function () {
+    /* @var \Tests\TestCase $this */
+
+    $this->actingAs($user = factory(User::class)->create(), 'api');
+    $post = factory(Post::class)->create(['id' => 123, 'user_id' => $user->id]);
+
+    $response = $this->post('/api/posts/' . $post->id . '/comment', [
+        'body' => 'A great comment here'
+    ]);
+    $response->assertOk();
+
+    $comment = Comment::first();
+
+    $this->assertCount(1, Comment::all());
+    $this->assertEquals($user->id, $comment->user_id);
+    $this->assertEquals('A great comment here', $comment->body);
+    $response->assertJson([
+        'data' => [
+            [
+                'data' => [
+                    'type' => 'comments',
+                    'comment' => 1,
+                    'attributes' => [
+                        'commented_by' => [
+                            'data' => [
+                                'attributes' => [
+                                    'name' => $user->name,
+                                    'user_id' => $user->id,
+                                ]
+                            ]
+                        ],
+                        'body' => 'A great comment here',
+                        'commented_at' => $comment->created_at->diffForHumans(),
+                    ],
+                ],
+                'links' => [
+                    'self' => url('/posts/123'),
+                ]
+            ]
+        ],
+        'links' => [
+            'self' => url('/posts'),
+        ]
+    ]);
+});

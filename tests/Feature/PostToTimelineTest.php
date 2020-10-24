b8c153fb47f588f5c38931133d0550630aa00991
Russkazkin <ruslan@skazkin.su>
2020-10-24 10:45:26 +0300

    Add a_user_can_post_a_text_post_with_an_image test

--- tests/Feature/PostToTimelineTest.php
+++ tests/Feature/PostToTimelineTest.php
@@ -6,11 +6,20 @@ use App\Models\Post;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class PostToTimelineTest extends TestCase
 {
     use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Storage::fake('public');
+    }
 
     /** @test */
     public function a_user_can_post_a_text_post()
@@ -43,6 +52,34 @@ class PostToTimelineTest extends TestCase
             ],
             'links' => [
                 'self' => url('/posts/' . $post->id),
+            ],
+        ]);
+    }
+
+    /** @test */
+    public function a_user_can_post_a_text_post_with_an_image()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->actingAs($user = factory(User::class)->create(), 'api');
+
+        $file = UploadedFile::fake()->image('user-post.jpg');
+
+        $response = $this->post('/api/posts', [
+            'body' => 'Testing Body',
+            'image' => $file,
+            'width' => 100,
+            'height' => 100,
+        ]);
+
+        Storage::disk('public')->assertExists('post-images/' . $file->hashName());
+
+        $response->assertStatus(201)->assertJson([
+            'data' => [
+                'attributes' => [
+                    'body' => 'Testing Body',
+                    'image' => url('post-images/' . $file->hashName()),
+                ],
             ],
         ]);
     }
